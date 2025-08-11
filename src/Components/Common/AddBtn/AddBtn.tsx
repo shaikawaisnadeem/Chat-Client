cdef756edf0c693c4104e675a8aa73c111f7f799
AwaisNadeem16 <awaisn.offi@gmail.com>
2025-08-11 23:55:59 +0530

    add

--- src/Components/Common/AddBtn/AddBtn.tsx
+++ src/Components/Common/AddBtn/AddBtn.tsx
@@ -4,7 +4,7 @@ import { MdAdd } from "react-icons/md";
 const AddBtn = () => {
     return (
         <div className='add-btn'>
-            <MdAdd size={15} color='#668f73ff' />
+            <MdAdd size={15}  />
         </div>
     )
 }
